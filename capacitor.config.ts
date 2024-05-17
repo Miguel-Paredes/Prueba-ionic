4ee6e3ae4db4f8457d1a2131224a111cc3101dd0
Miguel-Paredes <os.miguelparedes@gmail.com>
2024-05-17 16:58:59 -0500

    update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,8 +2,23 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'trabajo_1',
-  webDir: 'www'
+  appName: 'photo-gallery',
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      launchAutoHide: true,
+      backgroundColor: '#ffffff',
+      androidScaleType: 'CENTER_CROP',
+      showSpinner: false,
+      androidSpinnerStyle: 'large',
+      iosSpinnerStyle: 'small',
+      spinnerColor: '#999999',
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: 'launch_screen',
+    }
+  }
 };
 
 export default config;
